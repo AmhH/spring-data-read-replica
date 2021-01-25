43efdb621163ca5a39d08200d1dbe1d4e7528039
Amh <abaams78@gmail.com>
2021-01-25 10:14:17 -0600

    fields with particular annotation

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.example.controller;
 
 import com.example.model.Employee;
+import com.example.model.Person;
 import com.example.repository.EmployeeRepository;
 import com.example.repository.ReadEmployeeRepository;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.persistence.Transient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,5 +36,39 @@ public class EmployeeController {
   public void addEmployee(@RequestBody Employee employee){
     employeeRepository.save(employee);
   }
+
+  public static List<Field> getAllFields(List<Field> fields, Class<?> type) {
+    fields.addAll(
+        Arrays.stream(type.getDeclaredFields())
+            .filter(field -> field.isAnnotationPresent(Transient.class))
+            .collect(Collectors.toList())
+    );
+    if (type.getSuperclass() != null) {
+      getAllFields(fields, type.getSuperclass());
+    }
+    return fields;
+  }
+
+  public static void main(String[] args) {
+    List<Field> fieldList = new ArrayList<>();
+    List<Field> allFields = getAllFields(fieldList, Person.class);
+    System.out.println(fieldList);
+    System.out.println(allFields);
+  }
+  /**
+  public static void getAllFields(){
+    Reflections reflections = new Reflections(
+        new ConfigurationBuilder()
+            .setUrls(ClasspathHelper.forPackage("your.packageName"))
+            .setScanners(new SubTypesScanner(), new TypeAnnotationsScanner(), new FieldAnnotationsScanner()));
+
+    Set<Field> fields =
+        reflections.getFieldsAnnotatedWith(YourAnnotation.class);
+
+    fields.stream().forEach(field -> {
+      System.out.println(field.getDeclaringClass());
+      System.out.println(field.getName());
+    });
+  }*/
 
 }
